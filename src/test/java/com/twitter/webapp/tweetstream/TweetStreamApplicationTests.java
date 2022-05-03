c3c29c493dce74da99b179f0c83281fd96a0aa8e
Karan Parmar <40638530+parmar049@users.noreply.github.com>
2022-05-03 12:07:27 +0200

    Adding new project

---
+++ src/test/java/com/twitter/webapp/tweetstream/TweetStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitter.webapp.tweetstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
